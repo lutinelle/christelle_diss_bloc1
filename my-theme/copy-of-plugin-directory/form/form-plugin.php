454e1aae9970c5aeec1441a739d9052a37389c4f
christelle diss <lutinelle01@yahoo.fr>
2022-09-13 09:42:46 +0200

    copy of the plugin directory

---
+++ my-theme/copy-of-plugin-directory/form/form-plugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *Plugin Name: Form plugin
+ * Description: plugin to create and submit form
+ **/
+function form_plugin()
+{
+    $content = '';
+    $content .= '<form method="post" action="'.plugin_dir_url(__FILE__).'process/">';
+    $content .= '<div class="mb-3">';
+
+    $content .= '<label for="inputLastname" class="form-label">Nom</label>';
+    $content .= '<input type="text" class="form-control" id="inputLastname" name ="lastname">';
+    $content .= '</div>';
+
+    $content .= '<div class="mb-3">';
+    $content .= '<label for="inputName" class="form-label">Prénom</label>';
+    $content .= '<input type="text" class="form-control" id="inputName" name ="name">';
+    $content .= '</div>';
+    $content .= '<div class="mb-3">';
+    $content .= '<label for="InputEmail" class="form-label">Email</label>';
+    $content .= '<input type="email" class="form-control" id="InputEmail" name ="email">';
+    $content .= '</div>';
+    $content .= '<div class="mb-3">';
+    $content .= '<label for="inputNb" class="form-label">Numero de securité social</label>';
+    $content .= '<input type="text" class="form-control" id="inputNb" name ="nb">';
+    $content .= '</div>';
+    $content .= '<input type="submit" class="btn btn-info" name="submit_btn" value="Soumettre">';
+    $content .= '</div>';
+    $content .= '</form>';
+
+    return $content;
+}
+add_shortcode('form', 'form_plugin' );
+
---
+++ my-theme/copy-of-plugin-directory/form/process/index.php
@@ -0,0 +1,20 @@
+<?php
+$path = preg_replace('/wp-content.*$/','',__DIR__);
+$path.="wp-load.php";
+
+require_once ($path);
+
+if (isset($_POST['submit_btn']))
+{
+    $data=array(
+        'lastname' => $_POST['lastname'],
+        'name' => $_POST['name'],
+        'email' => $_POST['email'],
+        'nb' => $_POST['nb'],
+    );
+    $table_name = 'inscription';
+    $wpdb->insert($table_name, $data );
+    wp_redirect('http://wp.localhost/merci');
+}
+
+
